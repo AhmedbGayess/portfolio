d739487e8b56c7c07fd18a34418584e5bf133429
AhmedbGayess <38194881+AhmedbGayess@users.noreply.github.com>
2019-02-06 21:39:10 +0100

    production config

--- server/config/keys_prod.js
+++ server/config/keys_prod.js
@@ -1,8 +1,8 @@
 module.exports = {
-  mongoURI: process.env.MONGO_URI,
-  secretOrKey: process.env.SECRET_OR_KEY,
-  mailHost: process.env.MAIL_HOST,
-  mailUser: process.env.MAIL_USER,
-  mailPass: process.env.MAIL_PASS,
-  receivingMail: process.env.RECEIVING_MAIL
+  mongoURI: process.env.PORTFOLIO_MONGO_URI,
+  secretOrKey: process.env.PORTFOLIO_SECRET_OR_KEY,
+  mailHost: process.env.PORTFOLIO_MAIL_HOST,
+  mailUser: process.env.PORTFOLIO_MAIL_USER,
+  mailPass: process.env.PORTFOLIO_MAIL_PASS,
+  receivingMail: process.env.PORTFOLIO_RECEIVING_MAIL
 };
